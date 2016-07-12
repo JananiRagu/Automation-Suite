a9d60902cef0e0ddba0949b5e041ae31ebd77778
Janani <janani.ragu@supervalu.com>
2016-07-13 00:20:48 +0530

    Updates to Coupons

--- cub/src/test/main/com/test/regression/cub/pages/MyeCouponsPage.java
+++ cub/src/test/main/com/test/regression/cub/pages/MyeCouponsPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import com.test.regression.cub.utils.Constant;
 import com.test.regression.cub.utils.Logg;
 import com.test.regression.cub.utils.SuiteBase;
 
@@ -32,13 +33,14 @@ public class MyeCouponsPage extends SuiteBase{
 	@FindAll({@FindBy(xpath = "//div[@id='coupons-outer']/div/div/div/p/strong")})
 	private List<WebElement> _myECoupons;
 	
-	public void clickOnMyeCouponsInMyToolsHomePage() throws InterruptedException{
-		Thread.sleep(5000);
+	public void clickOnMyeCouponsInMyToolsHomePage(WebDriver driver) throws InterruptedException{
+		/*Thread.sleep(5000);
 		_MyToolsLink.click();
-		
+		waitFor(_MyECoupons);
 		Thread.sleep(5000);
-		_MyECoupons.click();
+		_MyECoupons.click();*/
 		
+		driver.navigate().to(Constant.URL+"savings/view-coupon-status.html");
 		log.info("Clicked on My eCoupons in Home page");
 	    
 	}
@@ -64,6 +66,7 @@ public class MyeCouponsPage extends SuiteBase{
 	
 	public int noOfECoupons(){
 		int size = _myECoupons.size();
+		log.info("No. of coupons in eCoupons page is : "+ size);
 		return size;
 	}
 	
