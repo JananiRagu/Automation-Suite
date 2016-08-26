56e00caee9644efd0e30d45105095fa51af7ed39
Janani <janani.ragu@supervalu.com>
2016-08-26 15:46:52 +0530

    added properties file

---
+++ cub/src/test/main/com/test/regression/cub/utils/ReadPropertiesFile.java
@@ -0,0 +1,40 @@
+package com.test.regression.cub.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadPropertiesFile {
+
+	String result = "";
+	FileInputStream inputStream;
+ 
+	public String getPropValue(String wantedPropertyValue) throws IOException {
+ 
+		try {
+			Properties prop = new Properties();
+			String propFileName = "config.properties";
+			
+			inputStream = new FileInputStream(new File(propFileName));
+ 
+			//inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+ 
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+ 
+			result = prop.getProperty(wantedPropertyValue);
+ 
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+		return result;
+	}
+}
